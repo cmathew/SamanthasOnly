ecf740ce72fa7111710cb75d308bd6c7a9832e81
cmathew <chris.w.mathew@gmail.com>
2018-09-26 17:03:56 -0700

    rough match layout

---
+++ app/src/main/java/com/cmathew/samanthasonly/MatchFragment.kt
@@ -0,0 +1,35 @@
+package com.cmathew.samanthasonly
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+private const val ARG_MATCH_ID = "match_id"
+
+class MatchFragment : Fragment() {
+	private var matchId: String? = null
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		arguments?.let {
+			matchId = it.getString(ARG_MATCH_ID)
+		}
+	}
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+														savedInstanceState: Bundle?): View? {
+		return inflater.inflate(R.layout.fragment_match, container, false)
+	}
+
+	companion object {
+		@JvmStatic
+		fun newInstance(matchId: String) =
+				MatchFragment().apply {
+					arguments = Bundle().apply {
+						putString(ARG_MATCH_ID, matchId)
+					}
+				}
+	}
+}

41e29fba29da3ff32aa43b94eadbfaade050a1c7
cmathew <chris.w.mathew@gmail.com>
2018-09-30 09:53:17 -0700

    set launcher icon appearance

--- app/src/main/java/com/cmathew/samanthasonly/CelebrateActivity.kt
+++ app/src/main/java/com/cmathew/samanthasonly/CelebrateActivity.kt
@@ -1,7 +1,8 @@
 package com.cmathew.samanthasonly
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.drawable.VectorDrawable
 import android.os.Bundle
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -11,24 +12,14 @@ import butterknife.ButterKnife.bind
 import com.github.jinatonic.confetti.ConfettiManager
 import com.github.jinatonic.confetti.ConfettiSource
 import com.github.jinatonic.confetti.ConfettoGenerator
-import com.github.jinatonic.confetti.Utils
 import com.github.jinatonic.confetti.confetto.BitmapConfetto
 import com.github.jinatonic.confetti.confetto.Confetto
 import java.util.*
-import android.graphics.drawable.VectorDrawable
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.drawable.Drawable
-import android.opengl.ETC1.getHeight
-import android.opengl.ETC1.getWidth
-import android.os.Build
-import android.annotation.TargetApi
-import android.content.Context
-import android.graphics.Canvas
-import kotlin.math.max
 
 
 class CelebrateActivity : AppCompatActivity() {
 	private val CONFETTI_DURATION_MS: Long = 2000
+	private val CONFETTI_SIZE = 130
 
 	@BindView(R.id.confettiContainer)
 	lateinit var confettiContainer: FrameLayout
@@ -48,9 +39,7 @@ class CelebrateActivity : AppCompatActivity() {
 	}
 
 	private fun initializeConfetti() {
-		val flameColor = ContextCompat.getColor(this, R.color.flame)
-//		val confettiShapes = Utils.generateConfettiBitmaps(intArrayOf(flameColor), 30)
-		val confettiShapes = arrayListOf(R.drawable.star, R.drawable.flame).map {
+		val confettiShapes = arrayListOf(R.drawable.star).map {
 			ContextCompat.getDrawable(this, it)
 		}.map {
 			getBitmap(it as VectorDrawable)
@@ -65,14 +54,14 @@ class CelebrateActivity : AppCompatActivity() {
 			}
 		}
 
-		val confettiSource = ConfettiSource(0, -50, confettiContainer.width, -50)
+		val confettiSource = ConfettiSource(0, -CONFETTI_SIZE, confettiContainer.width, -CONFETTI_SIZE)
 
 		confettiManager = ConfettiManager(this, generator, confettiSource, confettiContainer)
 				.setEmissionDuration(CONFETTI_DURATION_MS)
 				.setEmissionRate(60f)
-				.setVelocityX(0f, 10f)
-				.setVelocityY(150f, 70f)
-				.setRotationalVelocity(180f, 180f)
+				.setVelocityX(0f, 30f)
+				.setVelocityY(250f, 100f)
+				.setRotationalVelocity(90f, 50f)
 				.animate()
 	}
 
@@ -86,8 +75,8 @@ class CelebrateActivity : AppCompatActivity() {
 
 	private fun getBitmap(vectorDrawable: VectorDrawable): Bitmap {
 		val aspect = vectorDrawable.intrinsicWidth / vectorDrawable.intrinsicHeight
-		val bitmap = Bitmap.createBitmap(aspect * 56,
-				56 / aspect, Bitmap.Config.ARGB_8888)
+		val bitmap = Bitmap.createBitmap(aspect * CONFETTI_SIZE,
+				CONFETTI_SIZE / aspect, Bitmap.Config.ARGB_8888)
 		val canvas = Canvas(bitmap)
 		vectorDrawable.setBounds(0, 0, canvas.width, canvas.height)
 		vectorDrawable.draw(canvas)
